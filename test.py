bdaa1ef9e7c4e169a953ac877eda2cde8d7ff46a
vasodeaguua6611 <gabookgonzalezpolar@gmail.com>
2025-02-03 00:43:07 -0500

    TTS v0.1/Testfile added

--- test.py
+++ test.py
@@ -15,8 +15,7 @@ def speaknow():
     threading.Thread(target=speak_thread, daemon=True).start()
 
 root = Tk()
-
-obj = LabelFrame(root, text="   Write your Text! |  TTS Tool", font=("Verdana", 17), bd=1)
+obj = LabelFrame(root, text=" Write your Text! | TTS Tool", font=("Verdana", 17), bd=1)
 obj.pack(fill="both", expand="yes", padx=20, pady=10)
 
 lbl = Label(obj, text="Enter your text here! : ", font=("Verdana", 12))
@@ -26,10 +25,7 @@ textvab = StringVar()
 text = Entry(obj, textvariable=textvab, width=50, font=("Verdana", 12))
 text.pack(side=tk.LEFT, padx=10, pady=10)
 
-btn = Button(obj, text="pipis", font=("Verdana", 12), command=speaknow)
+btn = Button(obj, text="Speak", font=("Verdana", 12), command=speaknow)
 btn.pack(side=tk.LEFT, padx=10)
 
-root.title("Xuyaxaki's Vasotistic TTS")
-root.geometry("1000x400")
-root.resizable(False, False)
 root.mainloop()
